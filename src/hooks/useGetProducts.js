566b3a190052752c72cd03199bcfea337545883f
jugui93 <juanguillermo93@gmail.com>
2022-05-30 12:12:45 -0500

    Custom hooks

--- src/hooks/useGetProducts.js
+++ src/hooks/useGetProducts.js
@@ -1,8 +1,15 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 
-const useGetProducts = () => {
+const useGetProducts = (API) => {
+    const [products, setProducts] = useState([]);
 
+    useEffect(async()=>{
+        const response = await axios(API);
+        setProducts(response.data);
+    }, []);
+
+    return products;
 };
 
 export default useGetProducts;
