255135cba9a7b39a199565acd2e8dd3fc3679faf
jugui93 <juanguillermo93@gmail.com>
2022-06-08 10:08:45 -0500

    Remove products from cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useContext } from "react";
 import '@styles/Header.scss';
 import Menu from "@components/Menu.jsx";
+import MyOrder from "@containers/MyOrder.jsx";
 import menu from "@icons/icon_menu.svg";
 import logo from "@logos/logo_yard_sale.svg";
 import AppContext from '../context/AppContext'
@@ -8,10 +9,14 @@ import shoppingCart from "@icons/icon_shopping_cart.svg";
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
+  const [toggleOrders, setToggleOrders] = useState(false);
   const { state } = useContext(AppContext);
 
   const handleToggle = () => {
     setToggle(!toggle);
+  };
+  const handleToggleOrders = () => {
+    setToggleOrders(!toggleOrders);
   };
   return (
     <nav>
@@ -44,13 +49,14 @@ const Header = () => {
           <li className="navbar-email" onClick={handleToggle}>
             platzi@example.com
           </li>
-          <li className="navbar-shopping-cart">
+          <li className="navbar-shopping-cart" onClick={handleToggleOrders}>
             <img src={shoppingCart} alt="shopping cart" />
             {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
           </li>
         </ul>
       </div>
       {toggle && <Menu />}
+      {toggleOrders && <MyOrder />}
     </nav>
   );
 };
